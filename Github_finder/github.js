d487a524e2629e7660a51b0d0afa3c76413e1fdb
ccxcc233 <89978628+ccxcc233@users.noreply.github.com>
2021-09-22 09:02:24 +0300

    Add files via upload

---
+++ Github_finder/github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "367c064cf2abbb815947";
+    this.client_secret = "e9bf64875cd88e79a3cc495e59ff42dac2ba927a";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
